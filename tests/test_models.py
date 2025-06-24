c0ce765596af20af353f45d6eb817157ad05ff9f
Martin Uribe <clamytoe@gmail.com>
2025-06-24 13:22:03 -0500

    Added test for project model

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,9 @@
+def test_project_model_repr(app, make_project):
+    project = make_project(name="Alchemy", active=True)
+    with app.app_context():
+        assert repr(project) == f"<Project {project.project_name}>"
+
+
 def test_task_model_repr(app, make_task):
     task = make_task(task_desc="Test Task")
     with app.app_context():
