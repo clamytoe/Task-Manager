db9b4396f6fde17b28bcd5f1a9ea33a0c68042ef
Martin Uribe <clamytoe@gmail.com>
2025-06-25 06:41:06 -0500

    Removed unused code

--- task_manager/__init__.py
+++ task_manager/__init__.py
@@ -6,13 +6,12 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    # app.config.from_object("config.Config")
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ctm.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
     db.init_app(app)
 
     with app.app_context():
-        from . import models, rest_api, routes
+        from . import rest_api, routes
 
         db.create_all()
         app.register_blueprint(routes.routes)
